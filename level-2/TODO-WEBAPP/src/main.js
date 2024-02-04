f5e4123f5ffe7617e18258c74865d9383bfc9b46
moeketsi nelson <motubatsi.mn@gmail.com>
2024-02-04 21:26:56 +0200

    adding functionality to the delete btn

--- level-2/TODO-WEBAPP/src/main.js
+++ level-2/TODO-WEBAPP/src/main.js
@@ -5,81 +5,60 @@ const textinput = document.querySelector("#input-add");
 const pendingCont = document.querySelector(".pending-todo-container");
 const completedCont = document.querySelector(".completed-todo-container");
 
-// window.localStorage.setItem("pending", JSON.stringify({}));
-// window.localStorage.setItem("complete", JSON.stringify({}));
-
 addBtn.addEventListener("click", () => {
   if (textinput.value === "") return;
   let data = textinput.value;
   let id = uuidv4();
-  let obj = JSON.parse(window.localStorage.getItem("pending"));
-
-  if (obj === null) {
-    obj = {};
+  let obj = {};
+  if (!(localStorage.getItem("pending") === "empty")) {
+    obj = JSON.parse(localStorage.getItem("pending"));
   }
 
   obj[id] = data;
-  window.localStorage.setItem("pending", JSON.stringify(obj));
-  location.reload();
+  localStorage.setItem("pending", JSON.stringify(obj));
+
+  loadData();
 });
 
 function loadData() {
-  let pendingTodoObj = JSON.parse(window.localStorage.getItem("pending"));
-  let completedTodoObj = JSON.parse(window.localStorage.getItem("complete"));
+  if (!(localStorage.getItem("pending") === "empty")) {
+    let pendingTodoObj;
 
-  for (const todo in pendingTodoObj) {
-    createListItem(pendingCont, pendingTodoObj[todo], todo, "pending");
+    if (localStorage.getItem("pending") === "empty") {
+      pendingTodoObj = "empty";
+    } else {
+      pendingTodoObj = JSON.parse(localStorage.getItem("pending"));
+    }
+
+    while (pendingCont.hasChildNodes()) {
+      pendingCont.removeChild(pendingCont.lastChild);
+    }
+
+    for (const todo in pendingTodoObj) {
+      createListItem(pendingCont, pendingTodoObj[todo], todo, "pending");
+    }
   }
 
-  for (const todo in completedTodoObj) {
-    createListItem(completedCont, completedTodoObj[todo], todo, "complete");
+  if (!(localStorage.getItem("complete") === "empty")) {
+    let completedTodoObj;
+
+    if (localStorage.getItem("complete") === "empty") {
+      completedTodoObj = "empty";
+    } else {
+      completedTodoObj = JSON.parse(localStorage.getItem("complete"));
+    }
+
+    while (completedCont.hasChildNodes()) {
+      completedCont.removeChild(completedCont.lastChild);
+    }
+
+    for (const todo in completedTodoObj) {
+      createListItem(completedCont, completedTodoObj[todo], todo, "complete");
+    }
   }
 }
 
 loadData();
-
-attachEvent();
-
-function attachEvent() {
-  document.querySelectorAll("#check").forEach((checkInput) => {
-    checkInput.addEventListener("change", (e) => {
-      if (e.target.checked === true) {
-        //add logic to moved the item to the completed list
-        let id = e.target.parentElement.parentElement.id.slice(
-          1,
-          e.target.parentElement.parentElement.id.length - 2
-        );
-        let todoName = e.target.nextSibling.innerText;
-
-        let pendingTodoObj = JSON.parse(window.localStorage.getItem("pending"));
-        let completedTodoObj = JSON.parse(
-          window.localStorage.getItem("complete")
-        );
-
-        if (completedTodoObj === null) {
-          completedTodoObj = {};
-        }
-
-        completedTodoObj[id] = todoName;
-        delete pendingTodoObj[id];
-
-        window.localStorage.setItem("pending", JSON.stringify(pendingTodoObj));
-        window.localStorage.setItem(
-          "complete",
-          JSON.stringify(completedTodoObj)
-        );
-        console.log(id);
-        location.reload();
-        // setTimeout(() => {
-        //   createListItem(completedCont, todoName, id, "complete");
-        // }, 200);
-        // setTimeout(() => {
-        //   e.target.parentElement.parentElement.remove();
-        // }, 50);
-      }
-    });
-  });
-}
 
 function createListItem(cont, data, id, state) {
   let listItem = document.createElement("li");
@@ -97,8 +76,59 @@ function createListItem(cont, data, id, state) {
   name.className = "todo-name";
   name.innerText = data;
 
-  deleteBtn.addEventListener("click", () => {
-    deleteBtn.parentElement.remove();
+  deleteBtn.addEventListener("click", async (e) => {
+    let id = e.target.parentElement.id.slice(
+      1,
+      e.target.parentElement.id.length
+    );
+
+    let pendingTodoObj = JSON.parse(localStorage.getItem("pending"));
+    if (pendingTodoObj.hasOwnProperty(id)) {
+      delete pendingTodoObj[id];
+    }
+
+    let completedTodoObj = JSON.parse(localStorage.getItem("complete"));
+    if (completedTodoObj.hasOwnProperty(id)) {
+      delete completedTodoObj[id];
+    }
+
+    localStorage.setItem("pending", JSON.stringify(pendingTodoObj));
+    localStorage.setItem("complete", JSON.stringify(completedTodoObj));
+
+    loadData();
+  });
+
+  checkInput.addEventListener("change", (e) => {
+    if (e.target.checked === true) {
+      let id = e.target.parentElement.parentElement.id.slice(
+        1,
+        e.target.parentElement.parentElement.id.length
+      );
+      let todoName = e.target.nextSibling.innerText;
+
+      let pendingTodoObj = {};
+      let completedTodoObj = {};
+
+      if (localStorage.getItem("pending") === "empty") {
+        pendingTodoObj = {};
+      } else {
+        pendingTodoObj = JSON.parse(localStorage.getItem("pending"));
+      }
+
+      if (localStorage.getItem("complete") === "empty") {
+        completedTodoObj = {};
+      } else {
+        completedTodoObj = JSON.parse(localStorage.getItem("complete"));
+      }
+
+      completedTodoObj[id] = todoName;
+      delete pendingTodoObj[id];
+
+      localStorage.setItem("pending", JSON.stringify(pendingTodoObj));
+      localStorage.setItem("complete", JSON.stringify(completedTodoObj));
+
+      loadData();
+    }
   });
 
   if (state === "pending") {
